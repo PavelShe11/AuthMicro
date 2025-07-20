d6618b7f19cb83e20664fe1442d05555b8c0be74
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-07-20 13:49:23 +0300

    feat(database): db successfully build
    - create db structure (repo, entities)
    - db build localy

---
+++ src/main/java/io/github/pavelshe11/authmicro/store/entities/LoginSessionEntity.java
@@ -0,0 +1,34 @@
+package io.github.pavelshe11.authmicro.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "login_session")
+public class LoginSessionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "account_id", nullable = false)
+    private UUID accountId;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(name = "code_expires", nullable = false)
+    private Instant codeExpires;
+
+    @Builder.Default
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt = Instant.now();
+}
---
+++ src/main/java/io/github/pavelshe11/authmicro/store/repositories/LoginSessionRepository.java
@@ -0,0 +1,9 @@
+package io.github.pavelshe11.authmicro.store.repositories;
+
+import io.github.pavelshe11.authmicro.store.entities.LoginSessionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LoginSessionRepository extends JpaRepository<LoginSessionEntity, UUID> {
+}
